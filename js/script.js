bfbf6b3d38a9451d47781c393f811537624790dc
Artem <reebok001@yandex.ru>
2022-07-22 15:24:09 +0300

    Add section FAQ End fourth lesson

--- js/script.js
+++ js/script.js
@@ -79,7 +79,7 @@ const socket = new WebSocket('ws://web-socket-current.herokuapp.com');
 
 const renderExchange = (wrapper, data) => {
   const {from, to, rate, change} = JSON.parse(data);
-  // console.log(from, to, rate, change);
+  console.log(from, to, rate, change);
 
   const listItem = document.createElement('li');
   listItem.classList.add(
@@ -110,3 +110,59 @@ socket.addEventListener('message', event => {
 socket.addEventListener('error', err => {
   console.log(err)
 })
+
+//FAQ
+const hide = (elem, answer) => {
+  if (!elem.classList.contains('faq__item_show')) return;
+
+  answer.style.height = `${answer.offsetHeight}px`
+  answer.offsetHeight;
+  answer.style.display = 'block';
+  answer.style.height = 0;
+  answer.style.overflow = 'hidden';
+  answer.style.transition = 'height 0.2s ease-in-out';
+
+  elem.classList.remove('faq__item_show');
+
+  setTimeout(() => {
+    answer.style.display = '';
+    answer.style.height = '';
+    answer.style.overflow = '';
+    answer.style.transition = '';
+  }, 200)
+}
+
+const show = (elem, answer) => {
+  if (elem.classList.contains('faq__item_show')) return;
+
+  answer.style.display = 'block';
+  const height = answer.offsetHeight;
+  answer.style.height = 0;
+  answer.style.overflow = 'hidden';
+  answer.style.transition = 'height 0.2s ease-in-out';
+  answer.offsetHeight;
+  answer.style.height = `${height}px`
+
+  setTimeout(() => {
+    elem.classList.add('faq__item_show');
+    answer.style.display = '';
+    answer.style.height = '';
+    answer.style.overflow = '';
+    answer.style.transition = '';
+  }, 200)
+}
+
+const accordion = () => {
+  const list = document.querySelector('.faq__list');
+
+  list.addEventListener('click', e => {
+    const button = e.target.closest('.faq__question');
+    if (button) {
+      const item = button.closest('.faq__item');
+      const answer = item.querySelector('.faq__answer')
+      item.classList.contains('faq__item_show') ? hide(item, answer) : show(item, answer);
+    }
+  })
+}
+
+accordion();
